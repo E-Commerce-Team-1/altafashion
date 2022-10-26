9e8ac61b9297d45c93f878b424e10625317c7640
0Gravity94 <leesooyun94@gmail.com>
2022-10-26 17:37:18 +0800

    deploy conflict

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./utils/redux/store/store";
 
-import Index from "../src/routes/Index";
+import Index from "../src/routes/index";
 import "../src/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
